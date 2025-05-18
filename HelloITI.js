cba66d2daf9ad69b9a2c765eeb73ed3ffe3aea1d
Alaa Elnady <alaahamdialnadi@gmail.com>
2025-05-18 13:13:25 +0300

    Adding printHelloGit method to HelloITI JS file

--- HelloITI.js
+++ HelloITI.js
@@ -2,7 +2,12 @@ class HelloITI {
   printHelloITI() {
     return "Hello ITI";
   }
+
+  printHelloGit() {
+    return "Hello Git";
+  }
 }
 
 const obj1 = new HelloITI();
 console.log(obj1.printHelloITI());
+console.log(obj1.printHelloGit());
